6f254178423d20a994f3addcdf3aa33c78a1af5c
Favvie <favourprecious3@gmail.com>
2022-06-02 16:40:09 +0100

    Update task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-lists = dir(hidden_4)
-for i in lists:
-    if i[0] != "_":
-        print(i)
+if __name__ == "__main__":
+    import hidden_4
+    lists = dir(hidden_4)
+    for i in lists:
+        if i[0] != "_":
+            print(i)
